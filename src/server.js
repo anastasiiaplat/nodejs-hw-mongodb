1eb016bb0e942f24eb0fa7d9ae23d4be4770bdeb
anastasiiaplat <ana-plat@ukr.net>
2024-05-28 17:57:28 +0300

    done

--- src/server.js
+++ src/server.js
@@ -1,54 +1,80 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import mongoose from 'mongoose';
 import { env } from './utils/env.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
 
+export function setupServer() {
+  const app = express();
+  const PORT = Number(env('PORT', '3000'));
 
-const PORT = Number(env('PORT', '3001'));
+  app.use(express.json());
+  app.use(cors());
 
-export const setupServer = () => {
-    const app = express();
-    app.use(express.json());
-    app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-    app.use(
-        pino({
-            transport: {
-                target: 'pino-pretty',
-            },
-        }),
-    );
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllContacts();
+      res.status(200).json({
+        status: res.statusCode,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: res.statusCode,
+        message: 'Error retrieving contacts',
+        error: error.message,
+      });
+    }
+  });
 
-    app.get('/contacts', async (req, res) => {
-        const contacts = await getAllContacts();
-        res.status(200).json({
-            message: 'Successfully found contacts!',
-            data: contacts,
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+      return res.status(404).json({
+        message: 'Not found',
+      });
+    }
+
+    try {
+      const contact = await getContactById(contactId);
+      if (!contact) {
+        return res.status(404).json({
+          status: 404,
+          message: `Contact with id ${contactId} not found!`,
         });
-    });
+      }
 
-    app.get('/contacts/:contactId', async (req, res) => {
-        const contactId = req.params.contactId;
-        const contact = await getContactById(contactId);
-        if (!contact) {
-            return res.status(404).json({
-                message: `There is no contact with id ${contactId}`,
-            });
-        }
-        res.status(200).json({
-            message: `Successfully found contact with id ${contactId}`,
-            data: contact,
-        });
-    });
+      res.status(200).json({
+        status: res.statusCode,
+        message: `Successfully found contact with id: ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: res.statusCode,
+        message: 'Error retrieving contact',
+        error: error.message,
+      });
+    }
+  });
 
-    app.use('*', (req, res, next) => {
-        res.status(404).json({
-            message: 'Not found',
-        });
+  app.use('*', (req, res) => {
+    res.status(404).json({
+      message: 'Not found',
     });
+  });
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-};
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
